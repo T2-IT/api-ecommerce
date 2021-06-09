c555bb36e83452931431aadb8cc0ecd78c5b5bab
jgreis <jgreis.dev@protonmail.com>
2021-06-09 19:18:18 -0300

    Criação dos models Cliente e Endereço

--- src/main/java/br/org/serratec/projetoecommerce/model/Cliente.java
+++ src/main/java/br/org/serratec/projetoecommerce/model/Cliente.java
@@ -32,5 +32,126 @@ public class Cliente {
     @Column(name = "data_nasc")
     private LocalDate dataNasc;
 
-    
+    public Cliente() {
+    }
+
+    public Cliente(Long id, String email, String usuario, String nomeCompleto, String senha, String cpf,
+            String telefone, LocalDate dataNasc) {
+        this.id = id;
+        this.email = email;
+        this.usuario = usuario;
+        this.nomeCompleto = nomeCompleto;
+        this.senha = senha;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.dataNasc = dataNasc;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public LocalDate getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(LocalDate dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((usuario == null) ? 0 : usuario.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cliente other = (Cliente) obj;
+        if (cpf == null) {
+            if (other.cpf != null)
+                return false;
+        } else if (!cpf.equals(other.cpf))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (usuario == null) {
+            if (other.usuario != null)
+                return false;
+        } else if (!usuario.equals(other.usuario))
+            return false;
+        return true;
+    }
+
 }
--- src/main/java/br/org/serratec/projetoecommerce/model/Endereco.java
+++ src/main/java/br/org/serratec/projetoecommerce/model/Endereco.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+// import javax.persistence.JoinTable;
+// import javax.persistence.ManyToMany;
 
 @Entity
 public class Endereco {
@@ -28,6 +30,10 @@ public class Endereco {
 
     @Column(name = "estado")
     private String uf;
+
+    // @ManyToMany
+    // @JoinTable(name = "")
+    // public Cliente cliente;
 
     public Endereco() {
     }
