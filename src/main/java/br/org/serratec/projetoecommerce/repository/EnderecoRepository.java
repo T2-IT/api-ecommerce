e160b54eed7d43bea41fe34ca7b0a9ed71ec4c2c
jgreis <jgreis.dev@protonmail.com>
2021-06-09 19:20:32 -0300

    Criação do pacote repository

---
+++ src/main/java/br/org/serratec/projetoecommerce/repository/EnderecoRepository.java
@@ -0,0 +1,7 @@
+package br.org.serratec.projetoecommerce.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRepository extends JpaRepository<EnderecoRepository, Long> {
+    
+}
