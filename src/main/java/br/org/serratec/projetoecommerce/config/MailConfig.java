9124627b9360696337c43d6eefb8079b81370ed3
jgreis <jgreis.dev@protonmail.com>
2021-06-10 10:00:49 -0300

    Configuração para envio de email

---
+++ src/main/java/br/org/serratec/projetoecommerce/config/MailConfig.java
@@ -0,0 +1,40 @@
+package br.org.serratec.projetoecommerce.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@Configuration
+public class MailConfig {
+    
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void confirmacaoCadastroCliente(String para, String assunto, String texto) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom("");
+        simpleMailMessage.setTo(para);
+        simpleMailMessage.setSubject(assunto);
+        simpleMailMessage.setText("");
+        javaMailSender.send(simpleMailMessage);
+    }
+
+    public void exclusaoCadastroCliente(String para, String assunto, String texto) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom("");
+        simpleMailMessage.setTo(para);
+        simpleMailMessage.setSubject(assunto);
+        simpleMailMessage.setText("");
+        javaMailSender.send(simpleMailMessage);
+    }
+
+    public void confirmacaoCompra(String para, String assunto, String texto) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom("");
+        simpleMailMessage.setTo(para);
+        simpleMailMessage.setSubject(assunto);
+        simpleMailMessage.setText("");
+        javaMailSender.send(simpleMailMessage);
+    }
+}
