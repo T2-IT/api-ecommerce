2ec3906f84235a5aed6fef116d23add16d60705e
jgreis <jgreis.dev@protonmail.com>
2021-06-25 12:58:19 -0300

    .

--- src/main/java/br/org/serratec/projetoecommerce/config/AppConfig.java
+++ src/main/java/br/org/serratec/projetoecommerce/config/AppConfig.java
@@ -1,14 +1,21 @@
 package br.org.serratec.projetoecommerce.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfig {
-    
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+public class AppConfig implements WebMvcConfigurer {
+
+    // @Bean
+    // public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    // return new BCryptPasswordEncoder();
+    // }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD").allowCredentials(true);
     }
 }
